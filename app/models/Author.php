f1c7b3752100099020fa270419ca87eba808790b
bagher2g <bagher2g@gmail.com>
2019-03-18 04:25:36 +0330

    nullable

--- app/models/Author.php
+++ app/models/Author.php
@@ -7,11 +7,16 @@ class Author
 {
     /** @Id @Column(type="integer") @GeneratedValue **/
     protected $id;
+
     /** @Column(type="string") **/
     protected $name;
 
     /** @Column(type="string") **/
     protected $web;
+
+    /**
+     * @Column(type="date", nullable=true)
+     */
 
     public function getId()
     {
@@ -36,5 +41,15 @@ class Author
     public function getWeb()
     {
         return $this->web ;
+    }
+
+    public function setDate($date)
+    {
+       $this->date = $date ;
+    }
+
+    public function getDate()
+    {
+        return $this->date ;
     }
 }
